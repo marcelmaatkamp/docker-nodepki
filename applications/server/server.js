ff6f859cb3c023e61b07b9c1a8fd541ede3b6fc4
marcel <m.maatkamp@gmail.com>
2017-07-20 08:37:41 +0200

    example servers added

---
+++ applications/server/server.js
@@ -0,0 +1,15 @@
+const https = require("https");
+const fs = require("fs");
+
+var servername = process.env.SERVERNAME;
+
+const options = {
+  key: fs.readFileSync("/certs/"+servername+"/domain.key"),
+  cert: fs.readFileSync("/certs/"+servername+"/signed.crt"),
+  ca: fs.readFileSync("/certs/"+servername+"/chained.pem")
+};
+
+https.createServer(options, (req, res) => {
+  res.writeHead(200);
+  res.end("hello world\n");
+}).listen(443);
